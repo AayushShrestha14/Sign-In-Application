fdf9636a65a1c3f3711b4c85bfc948bc3fd80e47
aayushlalshrestha <mail2ayus@yahoo.com>
2023-08-15 22:02:41 +0545

    upgrade

--- app/src/main/java/com/aayush/signinapp/MainActivity.java
+++ app/src/main/java/com/aayush/signinapp/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.aayush.signinapp.R;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
